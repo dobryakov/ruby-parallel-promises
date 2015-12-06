ef9c0d5f01af530685918005b6c29b38dc44843e
Grigoriy Dobryakov <grigoriydobryakov@gmail.com>
2015-12-06 14:31:34 +0100

    stupid cycle

--- parallel.rb
+++ parallel.rb
@@ -4,27 +4,30 @@ class Parallel
 
   def initialize
     @results = []
+    @tasks_count = 0
+    @p = Concurrent::Promise.new{true}
     super
   end
 
   def run(array_of_blocks = [])
-    p = Concurrent::Promise.new{true}
+    @tasks_count = array_of_blocks.count
     array_of_blocks.each{|block|
-      p.then{block.call}.then{|result| @results.push result }
+      @p.then{block.call}.then{|result| @results.push result }
     }
-    p.execute
-    p
+    @p.execute
+    @p
   end
 
   def results
+    while (@tasks_count > @results.count) do end
     @results
   end
 
 end
 
 parallel = Parallel.new
-run = parallel.run([ lambda{2+2}, lambda{3+3} ]) #.then{p parallel.results}
+parallel.run([ lambda{2+2}, lambda{3+3} ])
 #p run.value
 #sleep 1
-#p parallel.results
+p parallel.results
 
